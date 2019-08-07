fd3508cddf3c2c1919142a109adf780919ce17d5
Victor Chung <victochung998@yahoo.com>
2019-08-07 15:12:16 -0700

    making progress on commenting each line of codes in HTML, CSS, and JS file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,18 @@
-$(document).ready(function () {
 
-  // 1. Initialize Firebase
+$(document).ready(function () {
+  (function () {
+
+    var clockElement = document.getElementById( "clock" );
+  
+    function updateClock ( clock ) {
+      clock.innerHTML = new Date().toLocaleTimeString();
+    }
+  
+    setInterval(function () {
+        updateClock( clockElement );
+    }, 1000);
+  
+  }());
   // web app's Firebase configuration
   var firebaseConfig = {
     apiKey: "AIzaSyCbvC9Lw2oHNuJb4O8c055ftEVNQy5Gsd4",
@@ -11,36 +23,48 @@ $(document).ready(function () {
     messagingSenderId: "423802686346",
     appId: "1:423802686346:web:608ca356177d2564"
   };
+  //initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
-  var database = firebase.database();
+  //create a variable called "database" to reference the Firebase database 
+      var database = firebase.database();
 
   // Capture Button Click for adding trains
   $("#addTrain").on("click", function (event) {
-    event.preventDefault();
+  //this line prevents the page from refreshing when a user hits "enter"
+      event.preventDefault();
 
-    // Grabbed values from text boxes/user input
+  // Grabbed values from text boxes/user input and store them in a variable called trainName.
     var trainName = $("#trainName").val().trim();
+  // Grabbed values from text boxes/user input and store them in a variable called destination.
     var destination = $("#destination").val().trim();
+  // Grabbed values from text boxes/user input and store them in a variable called firstTrain.
     var firstTrain = $("#firstTrain").val().trim();
+  // Grabbed values from text boxes/user input and store them in a variable called interval.
     var freq = $("#interval").val().trim();
 
-    // creates local "temporary" object for holding train data- use the push method to upload train data to the database
-    database.ref().push({
+  // creates local "temporary" object for holding train data- use the .push method to upload train data to the database. .ref refers to the path for saving data to the root directory. A Reference represents a specific location in your Database and can be used for reading or writing data to that Database location. This is s Firebase method.
+      database.ref().push({
+  //writing data to the trainName location in the database  
       trainName: trainName,
+  //writing data to the destination location in the database
       destination: destination,
+  //writing data to the firstTrain location in the database
       firstTrain: firstTrain,
+  //writing data to the frequency location in the database
       frequency: freq
     });
   });
 
 
-  // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+  // Firebase is constantly watching for data changes and when it does occurs, it will print them to console and to html, when a child is added,a list of items from the database is retrieved and then the childSnapShot will contain the required data. This codes behaves similarly to tht on('value")
   database.ref().on("child_added", function (childSnapshot) {
-// storing everything into a variable
+// storing data from childSnapShot into "trainName" variable.
     var newTrain = childSnapshot.val().trainName;
+//storing data from childSnapShot into "destination" variable
     var newLocation = childSnapshot.val().destination;
+//storing data from childSnapShot into "firstTrain" variable
     var newFirstTrain = childSnapshot.val().firstTrain;
+//storing data from childSnapShot into "frequency" variable   
     var newFreq = childSnapshot.val().frequency;
 
     // using moment.js to figure the time, First Time (pushed back 1 year to make sure it comes before current time)
@@ -70,12 +94,13 @@ $(document).ready(function () {
       ' </td><td>' + catchTrain +
       ' </td><td>' + tMinutesTillTrain + ' </td></tr>');
 
-    // Clear input fields
+    // Clear input fields/text box when done so to allow new input
     $("#trainName, #destination, #firstTrain, #interval").val("");
     return false;
   },
-    //Handle the errors
+    //Handle the errors, if there is an error that Firebase encounters, it will be stored in the "errorObject"
     function (errorObject) {
+   // if there is an error, this will print the error
       console.log("Errors handled: " + errorObject.code);
     });
 
